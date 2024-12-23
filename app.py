db42f959699da30a7e5eea6ebe60a1963d854aa0
Ana Rita da Cruz Gomes <ritagomes@users.noreply.huggingface.co>
2024-12-23 02:03:11 +0000

    Added UI

--- app.py
+++ app.py
@@ -13,3 +13,26 @@ our_query = "What is the current president of the USA?"
 #Last week langchain has recommended to use invoke function for the below please :)
 completion = llm.invoke(our_query)
 print(completion)
+
+
+#App UI starts here
+st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
+st.header("LangChain Demo")
+
+#Gets the user input
+def get_text():
+    input_text = st.text_input("You: ", key="input")
+    return input_text
+
+
+user_input=get_text()
+response = load_answer(user_input)
+
+submit = st.button('Generate')  
+
+#If generate button is clicked
+if submit:
+
+    st.subheader("Answer:")
+
+    st.write(response)

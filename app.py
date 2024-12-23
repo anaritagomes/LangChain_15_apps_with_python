824744610bcf679e0987e9c54f4b4ab591c58857
Ana Rita da Cruz Gomes <ritagomes@users.noreply.huggingface.co>
2024-12-23 02:16:41 +0000

    Fixed empty input

--- app.py
+++ app.py
@@ -5,29 +5,31 @@ import os
 api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 def load_answer(question):
-    llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2") # Model link : https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2
-    answer=llm.invoke(question)
+    if not question.strip():
+        return "Please enter a question."
+    llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2")  # Model link: https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2
+    answer = llm.invoke(question)
     return answer
 
 
-#App UI starts here
+# App UI starts here
 st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
 st.header("LangChain Demo")
 
-#Gets the user input
+# Gets the user input
 def get_text():
     input_text = st.text_input("You: ", key="input")
     return input_text
 
 
-user_input=get_text()
+user_input = get_text()
 response = load_answer(user_input)
 
 formatted_response = response.strip()  # Removes extra spaces or line breaks
-    
+
 submit = st.button('Generate')  
 
-#If generate button is clicked
+# If generate button is clicked
 if submit:
     st.subheader("Answer:")
     st.text_area("Response:", formatted_response, height=300)

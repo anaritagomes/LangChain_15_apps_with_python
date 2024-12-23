aaa229adf6264a1e17103530ed70623b8c276176
Ana Rita da Cruz Gomes <ritagomes@users.noreply.huggingface.co>
2024-12-23 01:54:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,13 @@
 import streamlit as st
-from langchain_openai import OpenAI
+from langchain.llms import HuggingFaceEndpoint
 
 api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
-def load_answer(question):
-    llm = OpenAI(model_name="gpt-3.5-turbo-instruct",temperature=0)
-    answer=llm.invoke(question)
-    return answer
+llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2") # Model link : https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2
 
-#App UI starts here
-st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
-st.header("LangChain Demo")
+# The LLM takes a prompt as an input and outputs a completion
+our_query = "What is the current president of the USA?"
 
-#Gets the user input
-def get_text():
-    input_text = st.text_input("You: ", key="input")
-    return input_text
-
-
-user_input=get_text()
-response = load_answer(user_input)
-
-submit = st.button('Generate')  
-
-#If generate button is clicked
-if submit:
-
-    st.subheader("Answer:")
-
-    st.write(response)
-
+#Last week langchain has recommended to use invoke function for the below please :)
+completion = llm.invoke(our_query)
